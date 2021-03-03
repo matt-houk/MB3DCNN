fe2d5f0b1495fccbd8fbd459449ce170a5c03283
matt-houk <matthouk2011@gmail.com>
2021-03-03 15:05:12 -0500

    Efficiency concerns resolved, preprocessing takes ~5 min per file.

--- dataProcessing.py
+++ dataProcessing.py
@@ -2,14 +2,11 @@ import glob, os
 import numpy as np
 import mne
 
-import cProfile
-
+SUBJECT = 1
 DURATION = 313
 XDIM = 7
 YDIM = 6
 IGNORE = ('EOG-left', 'EOG-central', 'EOG-right')
-
-pr = cProfile.Profile()
 
 def getIndex(raw, tagIndex):
 	return int(raw.annotations[tagIndex]['onset']*250)
@@ -21,16 +18,15 @@ def isEvent(raw, tagIndex, events):
 	return False
 
 def getSlice1D(raw, channel, dur, index):
-	if (type(channel) == int):
-		channel = raw.ch_names[channel]
-	return raw[channel][0][0][index:index+dur]	
+	return raw[channel][index:index+dur]	
 
 def getSliceFull(raw, dur, index):
+	data = raw.get_data()
 	trial = np.zeros((XDIM, YDIM, dur))
-	for channel in raw.ch_names:
+	for i, channel in enumerate(raw.ch_names):
 		if not channel in IGNORE:
 			x, y = convertIndices(channel)
-			trial[x][y] = getSlice1D(raw, channel, dur, index)
+			trial[x][y] = getSlice1D(data, i, dur, index)
 	return trial
 	
 def convertIndices(channel):
@@ -38,26 +34,35 @@ def convertIndices(channel):
 	yDict = {'EEG-Fz':0, 'EEG-0':1, 'EEG-1':1, 'EEG-2':1, 'EEG-3':1, 'EEG-4':1, 'EEG-5':2, 'EEG-C3':2, 'EEG-6':2, 'EEG-Cz':2, 'EEG-7':2, 'EEG-C4':2, 'EEG-8':2, 'EEG-9':3, 'EEG-10':3, 'EEG-11':3, 'EEG-12':3, 'EEG-13':3, 'EEG-14':4, 'EEG-Pz':4, 'EEG-15':4, 'EEG-16':5}
 	return xDict[channel], yDict[channel]
 
-data_files = glob.glob('../datasets/BCICIV_2a_gdf/*.gdf')
+data_files = glob.glob('../datasets/BCICIV_2a_gdf/*T.gdf')
 
 try:
-	raw = mne.io.read_raw_gdf(data_files[0], verbose='ERROR')
+	raw = mne.io.read_raw_gdf(data_files[SUBJECT], verbose='ERROR')
 except IndexError:
 	print("No data files found") 
 
 event_times = []
+event_types = []
+
+event_dict = {'769':1, '770':2, '771':3, '772':4}
 
 for i in range(len(raw.annotations)):
 	if (isEvent(raw, i, ('769', '770', '771', '772'))):
 		event_times.append(getIndex(raw, i))
+		event_types.append(event_dict[raw.annotations[i]['description']])
 
-data = np.empty((len(event_times), XDIM, YDIM, DURATION))
+key = np.array(event_types)
 
-print(len(event_times))
+data = np.zeros((len(event_times), XDIM, YDIM, DURATION))
 
-#for i, event in enumerate(event_times):
-pr.enable()
-data[0] = getSliceFull(raw, DURATION, event_times[0])
-pr.disable()
+for i, event in enumerate(event_times):
+	print("Processing the " + str(i) + "th trial")
+	data[i] = getSliceFull(raw, DURATION, event_times[i])
 
-pr.print_stats()
+fnBase = raw.filenames[0][(raw.filenames[0].find("/A0")+1):raw.filenames[0].find(".gdf")]
+
+fn0 = "../datasets/BCICIV_2a_processed/" + fnBase + ".npy"
+fn1 = "../datasets/BCICIV_2a_processed/" + fnBase + "Key.npy"
+
+np.save(fn1, key)
+np.save(fn0, data)
